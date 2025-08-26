e543302be83543814e2705ac55e25183453597ac
scratchmario123 <scratchmario123@gmail.com>
2025-08-26 19:08:54 +0800

    accepted

---
+++ src/CodeForces/CF_158A_NextRound.java
@@ -0,0 +1,24 @@
+package CodeForces;
+
+import java.util.Scanner;
+
+public class CF_158A_NextRound {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        sc.nextLine();
+        int cnt = 0;
+        int kScore = 0;
+        for (int i = 1; i <= n; i++) {
+            int score = sc.nextInt();
+            if (i <= k && score > 0) {
+                if (i == k) kScore = score;
+                cnt++;
+            } else if (i > k) {
+                if (score > 0 && kScore == score) cnt++;
+            } else break;
+        }
+        System.out.println(cnt);
+    }
+}
---
+++ src/CodeForces/CF_282A_BitPlusPlus.java
@@ -0,0 +1,18 @@
+package CodeForces;
+
+import java.util.Scanner;
+
+public class CF_282A_BitPlusPlus {
+    public static void main(String[] args) {
+        int x = 0;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String op = sc.nextLine();
+            if (op.contains("--")) x--;
+            else x++;
+        }
+        System.out.println(x);
+    }
+}
