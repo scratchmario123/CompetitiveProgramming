cdc4431429bf079b0b3d805706fbbb57e6d45ee2
scratchmario123 <scratchmario123@gmail.com>
2025-08-26 19:12:57 +0800

    accepted

---
+++ src/CodeForces/CF_50A_DominoPiling.java
@@ -0,0 +1,13 @@
+package CodeForces;
+
+import java.util.Scanner;
+
+public class CF_50A_DominoPiling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println((m*n-m*n%2)/2);
+
+    }
+}
